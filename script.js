2ba4cb176eae646902507d6841dce02f3a067abc
Ayush Sen <mr.ayushsen@gmail.com>
2024-03-30 11:20:21 +0530

    Update

--- script.js
+++ script.js
@@ -71,3 +71,17 @@ $(document).ready(function(){
         }
     });
 });
+
+// formfill
+
+<form name="contact" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
